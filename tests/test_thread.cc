b3f32bbf817cba7be4abadc08150a7f7892dc29e
kawakawaDe <fdsam@hotmail.com>
2021-02-02 13:37:51 +0000

    mutex add to log

--- tests/test_thread.cc
+++ tests/test_thread.cc
@@ -3,7 +3,8 @@
 sylar::Logger::ptr p_logger = SYLAR_LOG_ROOT();
 
 int count = 0;
-sylar::RWMutex s_mutex;
+//sylar::RWMutex s_mutex;
+sylar::Mutex m_mutex;
 
 void func1(){
     SYLAR_LOG_INFO(p_logger) << "name: " << sylar::Thread::GetName()
@@ -12,24 +13,36 @@ void func1(){
                              << " this.id: " << sylar::Thread::GetThis()->getID();
     //sleep(20);
    for(int i = 0; i < 100000; ++i) {
-        sylar::RWMutex::WriteLock lock(s_mutex);
+        sylar::Mutex::Lock lock_(m_mutex);
         ++count;
     }
 }
 
-void func2(){
+void fun2() {
+    while(true) {
+        SYLAR_LOG_INFO(p_logger) << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    }
+}
 
+void fun3() {
+    while(true) {
+        SYLAR_LOG_INFO(p_logger) << "========================================";
+    }
 }
 
 int main(int argc, char** argv) {
     SYLAR_LOG_INFO(p_logger) << "pthread init";
+    //YAML::Node root = YAML::LoadFile("/home/sam/Documents/Sylar/bin/conf/log2.yml");
+    //sylar::Config::LoadFromYaml(root);
     std::vector<sylar::Thread::ptr> v_thread;
-    for(int i = 0; i<5;i++){
-        sylar::Thread::ptr pt(new sylar::Thread(&func1, "name"+std::to_string(i)));
+    for(int i = 0; i<2;i++){
+        sylar::Thread::ptr pt(new sylar::Thread(&fun2, "name"+std::to_string(i * 2)));
+        sylar::Thread::ptr pt2(new sylar::Thread(&fun3, "name"+std::to_string(i * 2 + 1)));
         v_thread.push_back(pt);
+        v_thread.push_back(pt2);
     }
 
-    for(int i = 0; i<5;i++){
+    for(size_t i = 0; i < v_thread.size();i++){
         v_thread[i]->join();
     }
 
